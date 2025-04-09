5837a79d92ac21522058ed348188b8e2a86442ec
Aviva Ruben <aviva@rubenfamily.com>
2025-04-09 00:14:46 -0500

    disabling more analytics:
    commit 696e147fff6cb631641fd99bf1bce5611889e719
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Wed Apr 9 00:08:01 2025 -0500
    
        don't use sentry transport

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,9 +3,7 @@ import {nanoid} from 'nanoid/non-secure'
 import {logEvent} from '#/lib/statsig/statsig'
 import {add} from '#/logger/logDump'
 import {type MetricEvents} from '#/logger/metrics'
-import {bitdriftTransport} from '#/logger/transports/bitdrift'
 import {consoleTransport} from '#/logger/transports/console'
-import {sentryTransport} from '#/logger/transports/sentry'
 import {
   LogContext,
   LogLevel,
@@ -13,15 +11,15 @@ import {
   type Transport,
 } from '#/logger/types'
 import {enabledLogLevels} from '#/logger/util'
-import {isNative} from '#/platform/detection'
 
 const TRANSPORTS: Transport[] = (function configureTransports() {
   switch (process.env.NODE_ENV) {
-    case 'production': {
-      return [sentryTransport, isNative && bitdriftTransport].filter(
-        Boolean,
-      ) as Transport[]
-    }
+    // case 'production': {
+    //   return [sentryTransport, isNative && bitdriftTransport].filter(
+    //     Boolean,
+    //   ) as Transport[]
+    // }
+    case 'production':
     case 'test': {
       return []
     }
