7af430edd74dbc8c7b4326e27a00f3506eacb820
Aviva Ruben <aviva@rubenfamily.com>
2025-04-09 11:35:16 -0500

    remove analytics, improve direct fetch ui and perf
    commit cd5ba34096d7d0d83b279aaaed725c7991bc8c60
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Wed Apr 9 11:30:25 2025 -0500
    
        improve direct fetch peformance and ui
    
    commit 54bc155b4da8ad120abb0864a2957cd76efbf571
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Wed Apr 9 11:03:02 2025 -0500
    
        configurable geolocation
    
    commit 0ce84d99ab9a066490e2e5a44c6bd5b987fa591a
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Wed Apr 9 00:08:01 2025 -0500
    
        don't use sentry transport

--- src/state/geolocation.tsx
+++ src/state/geolocation.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import EventEmitter from 'eventemitter3'
 
-import {networkRetry} from '#/lib/async/retry'
 import {logger} from '#/logger'
 import {type Device, device} from '#/storage'
 
@@ -24,27 +23,8 @@ const onGeolocationUpdate = (
  * additional mod authorities.
  */
 export const DEFAULT_GEOLOCATION: Device['geolocation'] = {
-  countryCode: undefined,
+  countryCode: 'US',
   isAgeRestrictedGeo: false,
-}
-
-async function getGeolocation(): Promise<Device['geolocation']> {
-  const res = await fetch(`https://bsky.app/ipcc`)
-
-  if (!res.ok) {
-    throw new Error(`geolocation: lookup failed ${res.status}`)
-  }
-
-  const json = await res.json()
-
-  if (json.countryCode) {
-    return {
-      countryCode: json.countryCode,
-      isAgeRestrictedGeo: json.isAgeRestrictedGeo ?? false,
-    }
-  } else {
-    return undefined
-  }
 }
 
 /**
@@ -66,59 +46,15 @@ export function beginResolveGeolocation() {
    * In dev, IP server is unavailable, so we just set the default geolocation
    * and fail closed.
    */
-  if (__DEV__) {
-    geolocationResolution = new Promise(y => y({success: true}))
-    if (!device.get(['geolocation'])) {
-      device.set(['geolocation'], DEFAULT_GEOLOCATION)
-    }
-    return
+  geolocationResolution = new Promise(y => y())
+  if (device.get(['geolocation']) == undefined) {
+    device.set(['geolocation'], DEFAULT_GEOLOCATION)
   }
+}
 
-  geolocationResolution = new Promise(async resolve => {
-    let success = true
-
-    try {
-      // Try once, fail fast
-      const geolocation = await getGeolocation()
-      if (geolocation) {
-        device.set(['geolocation'], geolocation)
-        emitGeolocationUpdate(geolocation)
-        logger.debug(`geolocation: success`, {geolocation})
-      } else {
-        // endpoint should throw on all failures, this is insurance
-        throw new Error(`geolocation: nothing returned from initial request`)
-      }
-    } catch (e: any) {
-      success = false
-
-      logger.debug(`geolocation: failed initial request`, {
-        safeMessage: e.message,
-      })
-
-      // set to default
-      device.set(['geolocation'], DEFAULT_GEOLOCATION)
-
-      // retry 3 times, but don't await, proceed with default
-      networkRetry(3, getGeolocation)
-        .then(geolocation => {
-          if (geolocation) {
-            device.set(['geolocation'], geolocation)
-            emitGeolocationUpdate(geolocation)
-            logger.debug(`geolocation: success`, {geolocation})
-            success = true
-          } else {
-            // endpoint should throw on all failures, this is insurance
-            throw new Error(`geolocation: nothing returned from retries`)
-          }
-        })
-        .catch((e: any) => {
-          // complete fail closed
-          logger.debug(`geolocation: failed retries`, {safeMessage: e.message})
-        })
-    } finally {
-      resolve({success})
-    }
-  })
+export function setGeolocation(geolocation: Device['geolocation']) {
+  device.set(['geolocation'], geolocation)
+  emitGeolocationUpdate(geolocation)
 }
 
 /**
