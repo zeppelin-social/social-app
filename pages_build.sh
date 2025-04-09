7e923cf22da50ff426fcd6eaff9b02c88f14689d
Aviva Ruben <aviva@rubenfamily.com>
2025-04-09 00:02:30 -0500

    rewrite paths to support post linking?

--- pages_build.sh
+++ pages_build.sh
@@ -2,3 +2,7 @@
 
 yarn intl:build
 yarn build-web
+
+# build system outputs some srcs and hrefs like src="static/"
+# need to rewrite to be src="/static/" to handle non root pages
+sed -i 's/\(src\|href\)="static/\1="\/static/g' web-build/index.html
