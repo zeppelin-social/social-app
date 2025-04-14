25f47dc7a3012e5b2f4557e9d9e53357b6f07590
Aviva Ruben <aviva@rubenfamily.com>
2025-04-14 11:35:29 -0500

    youtube iframe embed support
    commit d0be1a371cf7eb9c44258a61a5eb67d00b17944c
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Mon Apr 14 11:35:04 2025 -0500
    
        copy web iframes and use deer social for iframe hosting

--- pages_build.sh
+++ pages_build.sh
@@ -6,3 +6,6 @@ yarn build-web
 # build system outputs some srcs and hrefs like src="static/"
 # need to rewrite to be src="/static/" to handle non root pages
 sed -i 's/\(src\|href\)="static/\1="\/static/g' web-build/index.html
+
+# we need to copy the static iframe html to support youtube embeds
+cp -r bskyweb/static/iframe/ web-build/iframe
