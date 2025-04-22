4a186b078ed24e65125472294f89a897a73efa58
Aviva Ruben <aviva@rubenfamily.com>
2025-04-21 23:32:21 -0500

    fix deer.social facet embeds
    commit c3dc610c51a098cb2116157ff6d8f20b3e531a05
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Mon Apr 21 23:31:04 2025 -0500
    
        add deer.social to bsky app urls

--- src/lib/strings/url-helpers.ts
+++ src/lib/strings/url-helpers.ts
@@ -91,7 +91,10 @@ export function toBskyAppUrl(url: string): string {
 }
 
 export function isBskyAppUrl(url: string): boolean {
-  return url.startsWith('https://bsky.app/')
+  return (
+    url.startsWith('https://bsky.app/') ||
+    url.startsWith('https://deer.social/')
+  )
 }
 
 export function isRelativeUrl(url: string): boolean {
