c273fee4b33c8b103c5a6bbc41b0045d116b3be3
Aviva Ruben <aviva@rubenfamily.com>
2025-04-28 23:45:29 -0500

    android builds that are semi useable
    commit a9a12884a78b578c747f2d75f4481823f412a7b3
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Mon Apr 28 22:24:35 2025 -0500
    
        add sideload android profile
    
    commit 53ebbc70474e9b7cdc9359ecee7f52c073fc078c
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Mon Apr 28 21:34:05 2025 -0500
    
        idents
    
    commit 94a6d9b0848f5631c7df98da3e594a57fea12828
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Sun Apr 27 22:58:54 2025 -0500
    
        update identifiers
    
    commit 676b03c66d142565730b4600b0f927afbdd05071
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Sun Apr 27 22:43:29 2025 -0500
    
        fix dialog popups n such!
    
    commit 5870d3dbff131338fd932cc8f51439ba41325b87
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Sun Apr 27 18:34:19 2025 -0500
    
        betterer
    
    commit 5a05aead824c64fcc099c91be203c3d73770cdf5
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Sun Apr 27 16:33:05 2025 -0500
    
        more image work
    
    commit b45c3d72527389e8c10e90a687897fc7bb669352
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Sun Apr 27 12:00:50 2025 -0500
    
        icons pt1
    
    commit 637aadb3b08221399eca1467e14dc46ceb56691c
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Fri Apr 25 21:59:40 2025 -0500
    
        avoid unavail api
    
    commit 33188fd6f876caa8ca8dde3e8fc1c67bb38fe44d
    Author: Aviva Ruben <aviva@rubenfamily.com>
    Date:   Fri Apr 25 20:38:07 2025 -0500
    
        add image to flake

--- flake.nix
+++ flake.nix
@@ -6,37 +6,54 @@
   };
 
   outputs =
-    { nixpkgs, flake-utils,  android-nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          android_sdk.accept_license = true;
-          allowUnfree = true;
-        };
-      };
-      pinnedJDK = pkgs.jdk17;
-      androidSdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
-        cmdline-tools-latest
-        build-tools-35-0-0
-        build-tools-34-0-0
-        platform-tools
-        platforms-android-35
-        emulator
-        cmake-3-22-1
-        ndk-26-1-10909125
-        ndk-28-0-13004108
-      ]);
-    in
-    with pkgs;
     {
-      packages = {
-        default = callPackage ./default.nix { };
-      };
-      devShells = {
-        default =
-          mkShell rec {
+      nixpkgs,
+      flake-utils,
+      android-nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
+        pinnedJDK = pkgs.jdk17;
+        androidSdk = android-nixpkgs.sdk.${system} (
+          sdkPkgs:
+          with sdkPkgs;
+          [
+            cmdline-tools-latest
+            build-tools-35-0-0
+            build-tools-34-0-0
+            platform-tools
+            platforms-android-35
+            emulator
+            cmake-3-22-1
+            ndk-26-1-10909125
+            ndk-28-0-13004108
+          ]
+          ++ nixpkgs.lib.optionals (system == "aarch64-darwin") [
+            system-images-android-35-google-apis-arm64-v8a
+            system-images-android-35-google-apis-playstore-arm64-v8a
+          ]
+          ++ nixpkgs.lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
+            system-images-android-35-google-apis-x86-64
+            system-images-android-35-google-apis-playstore-x86-64
+          ]
+        );
+      in
+      with pkgs;
+      {
+        packages = {
+          default = callPackage ./default.nix { };
+        };
+        devShells = {
+          default = mkShell rec {
             buildInputs = [
               androidSdk
               pinnedJDK
@@ -56,12 +73,18 @@
               crowdin-cli
               eas-cli
 
+              bundletool
+
               typescript
               typescript-language-server
 
             ];
 
+            shellHook = ''
+              export GRADLE_USER_HOME=~/.cache/gradle
+            '';
           };
-      };
-    });
+        };
+      }
+    );
 }
